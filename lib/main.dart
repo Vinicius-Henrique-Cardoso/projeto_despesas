ff61f4ed8a3ca0f599489e5c335c2cbcba7ea821
Vinicius Henrique Cardoso <vinicius.h.c.4455@gmail.com>
2023-05-12 13:35:12 -0300

    Criando e estilizando o container

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,30 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: _transactions.map((tr) {
                 return Card(
-                  child: Text(tr.title),
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    )
+                  ]),
                 );
               }).toList(),
             )
